264f002ace258122baca521feb03d23d9dbfa1e2
TechnocommieSparkle() <36112723+VisualX2@users.noreply.github.com>
2020-12-08 21:00:27 +0200

    Update catalog.js

--- OKR/laba4/catalog.js
+++ OKR/laba4/catalog.js
@@ -14,26 +14,26 @@ function build_category(uwu){
         catalog += "<img src = '" + element.images[0] + "' class='d-block user-select-none' width='100%' height='200' aria-label='Placeholder: Image cap' focusable='false' role='img' preserveAspectRatio='xMidYMid slice' viewBox='0 0 318 180' style='font-size:1.125rem;text-anchor:middle'>";
         catalog += "<div class = 'card-body'><p class='card-text'>" + element.productDescription + "</p></div>";
         catalog += "<div class = 'card-body'><h2>" + element.price + " грн.</h2><p>" + element.weight + " г.</p><h6></div>"
-        catalog += "<button type='button' class='btn btn-danger' onclick='add_to_cart(" + element.id +  ");'>В Корзину</button></div></div>"
+        catalog += "<button type='button' class='btn btn-danger' onclick='add_to_cart(" + element.id +  ");'>В Корзину</button></div>"
             
     });
 }
-catalog += `
+catalog += `</div>
 <h1>Classic pizza</h1>
 <div id = "second_category" style = "display: grid; grid-template-columns: repeat(4, 1fr); grid-column-gap: 10px;">
 `
 build_category(base.filter(element => element.categoryId ==2)||[]);
-catalog += `
+catalog += `</div>
 <h1>Delux pizza</h1>
 <div id = "third_category" style = "display: grid; grid-template-columns: repeat(4, 1fr); grid-column-gap: 10px;">
 `
 build_category(base.filter(element => element.categoryId ==3)||[]);
-catalog += `
+catalog += `</div>
 <h1>Snacks</h1>
 <div id = "fourth_category" style = "display: grid; grid-template-columns: repeat(4, 1fr); grid-column-gap: 10px;">
 `
 build_category(base.filter(element => element.categoryId ==4)||[]);
-catalog += `
+catalog += `</div>
 <h1>Drinks</h1>
 <div id = "fifth_category" style = "display: grid; grid-template-columns: repeat(4, 1fr); grid-column-gap: 10px;">
 `

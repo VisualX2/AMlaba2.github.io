a050d3b6e5d8a8fe4bdb0a2b86e0c1c2c5304122
TechnocommieSparkle() <36112723+VisualX2@users.noreply.github.com>
2020-12-14 20:47:50 +0200

    1

--- OKR/laba4/app.js
+++ OKR/laba4/app.js
@@ -1,12 +1,14 @@
 const routes = {
-  '#catalog' : catalog,
-  '#cart' : cart
+  '#catalog' : catalog
 };
 
 const rootDiv = document.getElementById('root');
 
 if(testABInt(window.location.hash)){
   rootDiv.innerHTML = pageDetailed(window.location.hash);
+}
+else if (window.location.hash === "#cart"){
+  rootDiv.innerHTML = build_cart();
 }
 else{
   rootDiv.innerHTML = routes[window.location.hash];
@@ -19,6 +21,9 @@ function testABInt(string)
 window.onpopstate = () => {
   if(testABInt(window.location.hash)){
     rootDiv.innerHTML = pageDetailed(window.location.hash);
+  }
+  else if (window.location.hash === "#cart"){
+    rootDiv.innerHTML = build_cart();
   }
   else{
     rootDiv.innerHTML = routes[window.location.hash];
@@ -57,4 +62,43 @@ function updateCartCount(){
   base = JSON.parse(localStorage.getItem("cart"))||[];
   let count = base.reduce((a,b) => a+b, 0);
   document.getElementById('cart-count').innerHTML = count;
+}
+function build_cart(){
+  let cart = `
+  <div class="card border-primary mb-3" style="max-width: 20rem;">
+    <div class="card-header">Header</div>
+    <div class="order-display__order">
+  `
+  let base = JSON.parse(sessionStorage.getItem("productList"))||[];
+  let arrayNum = JSON.parse(localStorage.getItem("cart"))||[];
+  let currentArray = [];
+  base.forEach (element => {
+      if (arrayNum[element.id - 1] !== "0"){
+          currentArray = base[arrayNum.indexOf(element)];
+          cart += `<div class=".order__main-info"><div class="order__logo"><img src="`+element.images+`" alt=""></div>
+          <div class="order__description">
+          <div class="order__information">
+          
+          <h3>`+ element.productName +`</h3>
+          
+          <p>` + element.productDescription + `</p>
+          <div class="order__size">
+              Стандарт
+          </div>
+          </div>
+          <div class="order__buy" id="pizzas.5">
+              <div class="order__price">`+ element.price +`</div>
+              <div class="order__amount-plus-minus">
+              <div class="order__amount-minus" onclick = "decrease(`+ element.id +`)">-</div>
+              <div class="order__amount">`+arrayNum[element.id - 1]+`
+              </div>
+              <div class="order__amount-plus"onclick = "increase(`+ element.id +`)">+</div>
+              </div>
+          </div>
+      </div>`;
+      }
+      cart += `</div>`
+
+  });
+  return cart;
 }

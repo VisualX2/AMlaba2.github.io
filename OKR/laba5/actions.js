f551af6f76cde031d4384351dbcc4f86b0327369
TechnocommieSparkle() <36112723+VisualX2@users.noreply.github.com>
2020-12-25 01:05:20 +0200

    1

---
+++ OKR/laba5/actions.js
@@ -0,0 +1,26 @@
+let baseact = JSON.parse(sessionStorage.getItem("actionList"))||[];
+let actions = `
+<h1>Actions.</h1>
+<div class = "jumbotron">
+`
+
+    baseact.forEach (element => {
+        actions += `
+        <div id = "action" class = "actions_main__element">
+        <div class="actions_logo"><img style = "max-width: 400px;" src="`+element.images+`" alt=""></div>
+        <div class="actions_description">
+                    <p>`+element.date+`</p>
+                    <p><em>`+element.actionName+`</em></p>
+                    <p>`+element.actionDescription+`</p>
+                    
+                    <button type="submit" class="btn btn-danger" onclick="return location.href = '#actions/`+ element.id +`'">Детали</button>
+                        
+                    
+                </div>
+        </div>
+        `     
+    });
+
+    actions += `</div>`;
+
+
---
+++ OKR/laba5/catalog.js
@@ -0,0 +1,40 @@
+let base = JSON.parse(sessionStorage.getItem("productList"))||[];
+let catalog = `
+<h1>Product Catalog.</h1>
+<div class = "jumbotron">
+<h1>Basic pizza</h1>
+<div id = "first_category" class = "pizzas_category"">
+`
+build_category(base.filter(element => element.categoryId ==1)||[]);
+function build_category(uwu){
+    uwu.forEach (element => {
+        catalog += `<div class = "card border-secondary mb-3" style = "max-width: 350px;">`
+    
+        catalog += "<div class = 'card-body'><h5 class = 'card-title'>" + element.productName + "</h5></div>";
+        catalog += `<img src = '` + element.images + `' onclick="return location.href = '#catalog/`+ element.id +`'" class='d-block user-select-none' width='100%' height='200' aria-label='Placeholder: Image cap' focusable='false' role='img' preserveAspectRatio='xMidYMid slice' viewBox='0 0 318 180' style='font-size:1.125rem;text-anchor:middle'>`;
+        catalog += "<div class = 'card-body'><p class='card-text'>" + element.productDescription + "</p></div>";
+        catalog += "<div class = 'card-body'><h2>" + element.price + " грн.</h2><p>" + element.weight + " г.</p><h6></div>"
+        catalog += "<button type='button' class='btn btn-danger' onclick='add_to_cart(" + element.id +  ");'>В Корзину</button></div>"
+            
+    });
+}
+catalog += `</div>
+<h1>Classic pizza</h1>
+<div id = "second_category" class = "pizzas_category">
+`
+build_category(base.filter(element => element.categoryId ==2)||[]);
+catalog += `</div>
+<h1>Delux pizza</h1>
+<div id = "third_category" class = "pizzas_category">
+`
+build_category(base.filter(element => element.categoryId ==3)||[]);
+catalog += `</div>
+<h1>Snacks</h1>
+<div id = "fourth_category" class = "pizzas_category">
+`
+build_category(base.filter(element => element.categoryId ==4)||[]);
+catalog += `</div>
+<h1>Drinks</h1>
+<div id = "fifth_category" class = "pizzas_category">
+`
+build_category(base.filter(element => element.categoryId ==5)||[]);
